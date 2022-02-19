2f1f76a249afeaf16ebd3dff89d98bc5b0f7216f
Mike Hart <mikebhart@outlook.com>
2022-02-19 19:07:26 +0000

    test curl script v1.2

--- theme/test-curl.php
+++ theme/test-curl.php
@@ -1,18 +1,8 @@
 <?php
 
-
-
-//ini_set("allow_url_fopen", 1);
 //$API_KEY = "1KF99C7HM8CBVDZU";
 
-// echo 'fopen';
-// $json = file_get_contents($url);
-// $obj = json_decode($json);
-// var_dump($obj);
-
 //$curl_url = "https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=" . $API_KEY;
-
-// $curl = curl_init();
 
 // curl_setopt_array($curl, array(
 //   CURLOPT_URL => $curl_url,
@@ -29,74 +19,45 @@
 //   ),
 // ));
 
-// $response = curl_exec($curl);
-// $err = curl_error($curl);
-
-// curl_close($curl);
-
-// if ($err) {
-//   echo "cURL Error #:" . $err;
-// } else {
-
-//      # convert json output to PHP array
-//   $array = json_decode($response, true);
-
-//   # output reponse from array back to command line
-//   echo $array["response"];
-
-// }
-
-
-// add_action('http_api_curl', function( $handle ){
-//     //Don't verify SSL certs
-
-
-//     //Use Charles HTTP Proxy
-//     curl_setopt($handle, CURLOPT_PROXY, "127.0.0.1");
-//     curl_setopt($handle, CURLOPT_PROXYPORT, 8888);
-//  }, 10);
-
- 
-
-
-
-// echo 'curl';
-
-
-// $ch = curl_init();
-// curl_setopt($ch, CURLOPT_URL,("https://www.alphavantage.co/query?function=TIME_SERIES_DAILY&symbol=MSFT&apikey=" . $API_KEY));
-// curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-// $server_output = curl_exec ($ch);
-// $result = json_decode($server_output);
-
-// curl_close ($ch);
-
-
-// var_dump($result);
-
-
-//echo 'curl';
-
- $url="https://api.nasdaq.com/api/quote/KRSTX/historical?assetclass=mutualfunds&fromdate=2022-02-15&limit=9999&todate=2022-02-16";
-
- $ch = curl_init();
- curl_setopt( $ch, CURLOPT_URL, $url );
- curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
- curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
- curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
-
- $result = curl_exec( $ch );
- curl_close( $ch );
-
- $result_decoded = json_decode( $result, true );
-
- var_dump($result_decoded);
-
-echo 'END';
-
 // curl -X POST "https://dataondemand.nasdaq.com/api/v1/trades/recurring" \
 // -H "accept: application/json" \
 // -H "Content-Type: application/json" \
 // -H "Authorization: Bearer __token__" \
 // -d '{"market_centers": ["string"], "sub_market_centers": ["string"], "symbols": ["AAPL"]}'
 
+$yesterday = date("Y-m-d", strtotime("-1 day"));
+$today = date("Y-m-d");
+
+$url = "https://api.nasdaq.com/api/quote/KRSTX/historical?assetclass=mutualfunds&fromdate=" . $yesterday . '&' . 'todate=' . $today;
+
+$ch = curl_init();
+curl_setopt( $ch, CURLOPT_URL, $url );
+curl_setopt( $ch, CURLOPT_RETURNTRANSFER, true );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYPEER, false );
+curl_setopt( $ch, CURLOPT_SSL_VERIFYHOST, false );
+
+$result = curl_exec( $ch );
+$err = curl_error( $ch );
+
+curl_close( $ch );
+
+if ( $err ) {
+
+    error_log( 'KREST PRICE UPDATE ERROR URL=' . $url . ' Error=' . $err, 0 );
+
+  } else {
+
+    $result_decoded = json_decode( $result, true );
+    var_dump( $result_decoded );    
+
+    $err_message = $result_decoded["status"]["bCodeMessage"][0]["errorMessage"];
+
+    if ( strlen( $err_message) > 0 )  {
+
+        error_log( 'KREST PRICE UPDATE ERROR URL=' . $url . ' Error=' . $err_message, 0 );
+        
+    }
+    
+    echo 'Complete';
+
+}
