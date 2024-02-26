b1605af38bac005fbb5715cc414d646c05e68a4b
Mike Hart <mikebhart@outlook.com>
2024-02-26 18:48:43 +0000

    add caching to timber 2.0

--- theme/functions.php
+++ theme/functions.php
@@ -12,24 +12,203 @@ require_once __DIR__ . '/vendor/autoload.php';
 
 Timber\Timber::init();
 
-if ( ! class_exists( 'Timber' ) ) {
+if ( !class_exists( 'Timber' ) ) {
 
 	echo 'Timber is not activated.';
 	exit;
 	
 }
 
-// if ( !class_exists('ACF')) {
-    
-//     echo 'ACF is not activated.';
-// 	exit;
-// }
-
-
-require_once __DIR__ . '/src/GenLiteSite.php';
-
-
 // Sets the directories (inside your theme) to find .twig files.
 Timber::$dirname = [ 'templates' ];
+
+use Timber\Site;
+
+class GenLiteSite extends Site {
+
+    function __construct() {
+
+        add_action( 'after_setup_theme', [ $this, 'theme_supports' ] );
+        add_filter( 'timber/context', [ $this, 'add_to_context' ] );
+        add_action( 'wp_enqueue_scripts', [$this, 'load_scripts_and_styles'] );
+        add_filter( 'document_title_parts', [ $this, 'modify_title_format'] );
+        add_action( 'login_enqueue_scripts', [ $this, 'admin_login_logo' ] );
+        add_filter( 'wp_mail_from_name', [ $this, 'mail_sender_name' ] );
+        add_action( 'enqueue_block_editor_assets', [ $this, 'setup_block_editor_assets' ] );
+        add_filter( 'document_title_separator', [ $this, 'setup_document_title_separator' ] );
+        add_filter( 'wp_robots', [ $this, 'setup_robots_follow'] );
+        add_filter( 'wp_sitemaps_add_provider', [ $this, 'remove_users_from_sitemap'], 10, 2 );
+        add_filter( 'wp_sitemaps_posts_query_args', [ $this, 'remove_pages_from_sitemap'], 11, 2);
+
+        apply_filters( 'timber/cache/mode', function () { return Timber\Loader::CACHE_OBJECT; } );
+    
+        remove_action('wp_head', 'print_emoji_detection_script', 7);
+        remove_action('wp_print_styles', 'print_emoji_styles');
+        remove_action( 'admin_print_scripts', 'print_emoji_detection_script' );
+        remove_action( 'admin_print_styles', 'print_emoji_styles' );
+
+        parent::__construct();
+
+    }
+
+    function load_scripts_and_styles() {
+
+        $theme = wp_get_theme();
+        $app_version = $theme->Version;
+
+        wp_enqueue_style( 'genlite_style', get_template_directory_uri() . '/dist/main.css', array(), $app_version );
+        wp_enqueue_script( 'genlite_script', get_template_directory_uri() . '/dist/main.js', array(), $app_version );
+
+        if ( !is_user_logged_in() ) {
+
+            wp_dequeue_script( 'jquery' );
+            wp_deregister_script( 'jquery' ); 
+            wp_dequeue_style( 'classic-theme-styles' );
+
+        }
+
+    }
+
+    function add_to_context( $context ) {
+
+        $context['site']  = $this;
+        $context['body_class'] = implode(' ', get_body_class());
+       
+        // $context['header_menu'] = new TimberMenu('header menu');
+
+        $context['header_menu'] = Timber::get_menu('header menu');
+
+    
+        // Options
+        $option_fields = get_fields('options');
+
+        if ( $option_fields != null ) {
+            
+            $context['general'] = $option_fields['general'];
+            $context['footer'] = $option_fields['footer'];
+            $context['contact_form'] = $option_fields['contact_form'];
+            $context['script_code'] = $option_fields['script_code'];
+        }
+
+        return $context;
+    }
+
+    function theme_supports() {
+
+        require_once get_template_directory() . '/includes/register-post-types.php';
+        require_once get_template_directory() . '/includes/register-blocks.php';
+        require_once get_template_directory() . '/includes/admin-cleanup.php';
+    
+    
+        // Add Theme Support 
+        add_theme_support( 'title-tag' );   
+        add_theme_support( 'automatic-feed-links' );
+        add_theme_support( 'woocommerce' );
+        add_theme_support( 'post-thumbnails' );
+        add_theme_support( 'menus' );
+        add_theme_support( 'html5', ['comment-form', 'comment-list', 'gallery',	'caption'] );
+        add_theme_support( 'post-formats', ['aside','image','video','quote','link',	'gallery','audio'] );
+        add_theme_support( 'editor-styles' );
+        add_theme_support( 'wp-block-styles' );
+
+        if( function_exists('acf_add_options_page') ) {
+
+            acf_add_options_page([
+                'page_title'    => 'Theme Settings',
+                'menu_title'    => 'Theme Settings',
+                'menu_slug'     => 'genlite-theme-settings',
+                'capability'    => 'edit_posts',
+                'redirect'      => false
+            ]);
+        }
+
+    }
+
+    function document_title_separator( $sep ) {
+
+        $sep = " | ";
+
+        return $sep;
+
+    }
+
+
+    function modify_title_format( $title ) {
+
+        if ( !is_front_page() ) {
+
+            $title_parts['site'] = $title['title'];
+            $title_parts['title'] = $title['site'];
+        
+        } else {
+        
+            $title_parts['title'] = 'Freelance WordPress Developer | Mike Hart';
+
+        }
+        
+        return $title_parts;
+
+    }
+
+    function setup_block_editor_assets() {
+        wp_enqueue_style('genlite_editor_style', get_template_directory_uri() . '/editor-styles.css' );
+    }
+
+    function admin_login_logo() { 
+
+        $general = get_field( 'general', 'options' );
+        $login_form_logo = $general['logo']['url'];
+
+        ?>
+
+        <style type="text/css">
+            #login h1 a, .login h1 a {
+                background: url(<?php echo $login_form_logo; ?>);
+                background-repeat: no-repeat;
+                background-size: 200px 80px;         
+                width: 100%;
+                background-position-x: center;
+                pointer-events: none;
+                cursor: default;
+            }
+        </style>
+    <?php }
+
+    function mail_sender_name( $original_email_from ) {
+        return strtoupper( get_bloginfo() );
+    }
+
+    function setup_document_title_separator( $sep ) {
+        $sep = "|";
+
+        return $sep;
+    }
+
+    function setup_robots_follow( $robots ) {
+        $robots['follow'] = true;
+        return $robots;
+    }
+
+    function remove_users_from_sitemap( $provider, $name ) {
+
+        if ( 'users' === $name ) {
+            return false;
+        }
+
+        return $provider;
+    }
+
+    function remove_pages_from_sitemap( $args, $post_type ) {
+
+        if ( 'page' !== $post_type ) return $args;
+
+        $args['post__not_in'] = isset( $args['post__not_in'] ) ? $args['post__not_in'] : array();
+        $args['post__not_in'][] = 819; // contact-response1
+
+        return $args;
+    }
+
+}
+
 
 new GenLiteSite();
