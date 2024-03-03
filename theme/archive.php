3ebee822a31c3579df9b39e86fb6c065f1d886c5
Mike Hart <mikebhart@outlook.com>
2024-03-03 00:44:24 +0000

    fix comments

--- theme/archive.php
+++ theme/archive.php
@@ -1,10 +1,6 @@
 <?php
 
 $context = Timber::context();
-
-// $timber_post     = new Timber\Post();
-// $context['post'] = $timber_post;
-
 $context['post'] = Timber::get_posts();
 
 Timber::render( [ 'archive.twig' ], $context, 600, Timber\Loader::CACHE_OBJECT );
